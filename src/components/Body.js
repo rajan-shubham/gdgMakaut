fa3128bcdff40eccd3438f8e03525cef9f3b3924
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-11-29 01:54:10 +0530

    GDG MAKAUT HQ

---
+++ src/components/Body.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+const Body = () => {
+    const [searchText, setSearchText] = useState("");
+
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://cse.google.com/cse.js?cx=6232bdbd51c464e5a";
+        script.async = true;
+        document.getElementById("google-search-container").appendChild(script);
+    }, []);
+
+    return (
+        <div>
+            <div id="google-search-container" className="m-8 p-10">
+                <div className="gcse-search"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    return (
+        <div className="bg-gray-900 fixed top-0 w-full flex justify-between text-white z-10">
+            <div className="flex justify-center items-center space-x-6">
+                <h1 className="py-2 px-4 text-4xl">GDG MAKAUT</h1>
+            </div>
+                <div className="bg-gray-900 py-4 flex justify-end">
+                    <ul className="flex space-x-6">
+                        <li className="hover:bg-sky-500 py-2 px-4 rounded-lg"><a href="#about">Home</a></li>
+                        <li className="hover:bg-sky-500 py-2 px-4 rounded-lg"><a href="#skill">Service</a></li>
+                        <li className="hover:bg-sky-500 py-2 px-4 rounded-lg"><a href="#contact">Contact</a></li>
+                        <li className="hover:bg-sky-500 py-2 px-4 rounded-lg"><a href="#contact">Log In</a></li>
+                    </ul>
+                </div>
+        </div>
+    );
+};
+
+export default Header;
